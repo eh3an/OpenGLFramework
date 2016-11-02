6a00573284ba983f22ceb968c18ff2a7253e45fd
eh3an <eh3an@users.noreply.github.com>
2016-11-02 17:09:25 +0100

    Add files via upload

--- application/include/application_solar.hpp
+++ application/include/application_solar.hpp
@@ -8,6 +8,20 @@
 // gpu representation of model
 class ApplicationSolar : public Application {
  public:
+  // defining struct with properties for the planets
+  class Planet
+{
+   public:
+      float rot;
+      float trans;
+      float scale;
+      bool hasMoon;
+      glm::fmat4 model_matrix;
+      glm::fmat4 normal_matrix ;
+};
+
+
+
   // allocate and initialize objects
   ApplicationSolar(std::string const& resource_path);
   // free allocated objects
@@ -17,21 +31,24 @@ class ApplicationSolar : public Application {
   void uploadUniforms();
   // update projection matrix
   void updateProjection();
+  void updateProjection(int i);
   // react to key input
   void keyCallback(int key, int scancode, int action, int mods);
   //handle delta mouse movement input
   void mouseCallback(double pos_x, double pos_y);
-
+  //void glfwGetCursorPos( double xpos, double ypos);
   // draw all objects
   void render() const;
+  void upload_planet_transforms(Planet  &planet) const;
 
  protected:
   void initializeShaderPrograms();
-  void initializeGeometry();
+  void initializeGeometry(model& mdl, model_object& object);
+  void updateView(int i);
   void updateView();
-
   // cpu representation of model
   model_object planet_object;
+  model_object star_object;
 };
 
 #endif
